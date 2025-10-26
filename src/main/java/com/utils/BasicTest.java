d9f633a087c92f37fcf1a9366fe47f78039e4635
Xuân <huadongxuan852@gmail.com>
2025-10-26 17:14:11 +0700

    Update BasicTest, Constants and Smoke.xml for Firefox support

--- src/main/java/com/utils/BasicTest.java
+++ src/main/java/com/utils/BasicTest.java
@@ -3,10 +3,17 @@ package com.utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import com.beust.jcommander.Parameter;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.concurrent.TimeUnit;
 
@@ -18,17 +25,36 @@ public abstract class BasicTest {
     // private String driverPath;
 
     @BeforeMethod
+    @Parameters({"browser"})
     public void preCondition() {
         // Chromedriver path
         // driverPath = "src/main/resources/WebDrivers/chromedriver.exe";
         // ChromeOptions options = new ChromeOptions();
         // System.setProperty("webdriver.chrome.driver", driverPath);
         // driver = new ChromeDriver(options);
+       
+        String browser = Constants.browser;
+
+        if(browser.equalsIgnoreCase("chrome")) {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        // Maximize the browser
+
+        } else if(browser.equalsIgnoreCase("edge")) {
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+        
+        } else if(browser.equalsIgnoreCase("firefox")) {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+            
+
+        } else {
+            System.out.println("Browser is not supported: " + browser);
+            throw new RuntimeException("Invalid browser name in XML");
+        }
+
         driver.manage().window().maximize();
-        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @AfterMethod
